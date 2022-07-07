8e1515e6860f6ac5e727e6fbaad1266863264447
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-07-07 05:39:17 +0200

    Add Field Color

--- lib/Factories/colors_factory.dart
+++ lib/Factories/colors_factory.dart
@@ -6,7 +6,7 @@ class ColorsFactory {
   static const Color secondary = Color(0xFFFFFFFF);
   static const Color text = Color(0xFF000000);
   static const Color secondaryText = Color(0xFF787878);
-  static const Color tertiaryText = Color(0xFFFFFFFF);
+  static const Color field = Color(0xFFF1F1F1);
   static const Color hyperlink = Color(0xFF1777F2);
   static const Color success = Color(0xFF4BB543);
   static const Color danger = Color(0xFFDE0A0A);
