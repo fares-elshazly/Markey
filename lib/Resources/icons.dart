3e24742d00000eea326a5d45bfdc3842dc473486
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-07-07 05:38:14 +0200

    Add Social Buttons Icons Assets Resources

--- lib/Resources/icons.dart
+++ lib/Resources/icons.dart
@@ -1,3 +1,9 @@
 abstract class MRKIcons {
   static const _baseUrl = 'assets/icons';
+
+  static const google = '$_baseUrl/google.svg';
+  static const apple = '$_baseUrl/apple.svg';
+  static const facebook = '$_baseUrl/facebook.svg';
+  static const instagram = '$_baseUrl/instagram.svg';
+  static const twitter = '$_baseUrl/twitter.svg';
 }
