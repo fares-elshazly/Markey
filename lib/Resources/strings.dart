0fe6eaafd707c8bea2f6c5da0669c0c3b1208478
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-07-07 05:38:25 +0200

    Add Login Screen Resources

--- lib/Resources/strings.dart
+++ lib/Resources/strings.dart
@@ -1,4 +1,12 @@
 abstract class MRKStrings {
+  // * Login Screen
+  static const loginUsername = 'login.username';
+  static const loginPassword = 'login.password';
+  static const loginForgotPassword = 'login.forgot.password';
+  static const loginSubmit = 'login.submit';
+  static const loginDontHaveAccount = 'login.already.member';
+  static const loginRegister = 'login.register';
+
   // * Validation Messages
   static const successTitle = 'success.title';
   static const errorTitle = 'error.title';
