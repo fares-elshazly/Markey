864cdce90c9fc1f740e2f3f982568a712df5d9b7
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-07-07 05:39:10 +0200

    Add Initial Splash Screen

--- lib/Screens/Shared/splash.dart
+++ lib/Screens/Shared/splash.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SplashScreen extends StatelessWidget {
+import '/Screens/Authentication/login.dart';
+import '/Resources/images.dart';
+
+class SplashScreen extends StatefulWidget {
   static const routeName = '/Splash';
 
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  final _bodyHorizontalMargin = 15.0;
+
+  @override
+  void initState() {
+    _loadData();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(horizontal: _bodyHorizontalMargin),
+        child: _buildLogo(),
+      ),
+    );
+  }
+
+  Widget _buildLogo() {
+    return Center(
+      child: Image.asset(
+        MRKImages.logo,
+        fit: BoxFit.scaleDown,
+      ),
+    );
+  }
+
+  Future<void> _loadData() async {
+    await Future.delayed(const Duration(seconds: 3));
+    Get.toNamed(LoginScreen.routeName);
   }
 }
