58a7bb798577be7831ca6d9e82e55d2337070a17
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-07-07 05:39:30 +0200

    Add Login Screen

---
+++ lib/Screens/Authentication/login.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+import '/Widgets/Shared/custom_text_field.dart';
+import '/Widgets/Shared/submit_button.dart';
+import '/Widgets/Authentication/social_buttons.dart';
+import '/Widgets/Authentication/question_navigation.dart';
+import '/Resources/strings.dart';
+import '/Resources/images.dart';
+import '/Factories/text_factory.dart';
+import '/Utilities/validator.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const routeName = '/Login';
+
+  LoginScreen({Key? key}) : super(key: key);
+
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  final _bodyHorizontalMargin = 15.0;
+  final _bodyVerticalMargin = 15.0;
+  final _contentMargin = 20.0;
+  final _internalMargin = 15.0;
+  final _bottomBarPadding = 15.0;
+
+  final _logoHeight = 250.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(horizontal: _bodyHorizontalMargin),
+        child: _buildContent(),
+      ),
+      bottomNavigationBar: _buildBottomBar(),
+    );
+  }
+
+  Widget _buildContent() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(height: _bodyVerticalMargin),
+          _buildLogo(),
+          SizedBox(height: _contentMargin),
+          _buildForm(),
+          SizedBox(height: _contentMargin * 2),
+          _buildSocialButtons(),
+          SizedBox(height: _bodyVerticalMargin),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLogo() {
+    return Image.asset(
+      MRKImages.logo,
+      height: _logoHeight,
+      fit: BoxFit.scaleDown,
+    );
+  }
+
+  Widget _buildForm() {
+    return Form(
+      key: _formKey,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: _buildFormContent(),
+    );
+  }
+
+  Widget _buildFormContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        _buildUsernameField(),
+        SizedBox(height: _internalMargin),
+        _buildPasswordField(),
+        SizedBox(height: _internalMargin),
+        _buildForgotPassword(),
+        SizedBox(height: _internalMargin * 2),
+        _buildSubmit(),
+      ],
+    );
+  }
+
+  Widget _buildUsernameField() {
+    return CustomTextField(
+      hint: MRKStrings.loginUsername,
+      controller: _usernameController,
+      validator: Validator.isEmail,
+    );
+  }
+
+  Widget _buildPasswordField() {
+    return CustomTextField(
+      hint: MRKStrings.loginPassword,
+      controller: _passwordController,
+      obscured: true,
+      validator: Validator.isNotEmpty,
+    );
+  }
+
+  Widget _buildForgotPassword() {
+    return InkWell(
+      onTap: _forgotPassword,
+      child: TextFactory.buildNormalText3(
+        MRKStrings.loginForgotPassword,
+        weight: FontWeights.medium,
+      ),
+    );
+  }
+
+  Widget _buildSubmit() {
+    return SubmitButton(
+      label: MRKStrings.loginSubmit,
+      behaviour: _submit,
+    );
+  }
+
+  Widget _buildSocialButtons() {
+    return const SocialButtons();
+  }
+
+  Widget _buildBottomBar() {
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.all(_bottomBarPadding),
+        child: _buildRegister(),
+      ),
+    );
+  }
+
+  Widget _buildRegister() {
+    return QuestionNavigation(
+      question: MRKStrings.loginDontHaveAccount,
+      label: MRKStrings.loginRegister,
+      behaviour: _register,
+    );
+  }
+
+  void _forgotPassword() {}
+
+  void _submit() {}
+
+  void _register() {}
+}
