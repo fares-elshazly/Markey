2e9cba32acc3c56c584bb26d6de0545e828ca0fc
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-07-29 13:07:29 +0200

    Add DateTime Formatters Helpers Methods

--- lib/Utilities/helpers.dart
+++ lib/Utilities/helpers.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
 import 'package:timeago/timeago.dart' as timeago;
+import 'package:intl/intl.dart';
 
 abstract class Helpers {
   static const _placeHolder = '-';
@@ -22,5 +23,23 @@ abstract class Helpers {
   static String timeAgo(DateTime? dateTime) {
     if (dateTime == null) return _placeHolder;
     return timeago.format(dateTime);
+  }
+
+  static String previewDateTime(DateTime? dateTime) {
+    if (dateTime == null) return _placeHolder;
+    final now = DateTime.now();
+    final difference = now.difference(dateTime).inDays;
+    if (difference == 0) return formatTime(dateTime);
+    return formatDate(dateTime);
+  }
+
+  static String formatDate(DateTime? dateTime) {
+    if (dateTime == null) return _placeHolder;
+    return DateFormat.yMMMMd().format(dateTime);
+  }
+
+  static String formatTime(DateTime? dateTime) {
+    if (dateTime == null) return _placeHolder;
+    return DateFormat.jm().format(dateTime);
   }
 }
