566c0d1895d579d43de797d2f0d98d401c518193
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-07-29 11:06:55 +0200

    Add Home Colors

--- lib/Factories/colors_factory.dart
+++ lib/Factories/colors_factory.dart
@@ -10,4 +10,8 @@ class ColorsFactory {
   static const Color hyperlink = Color(0xFF1777F2);
   static const Color success = Color(0xFF4BB543);
   static const Color danger = Color(0xFFDE0A0A);
+  static const Color rate = Color(0xFFFFD700);
+
+  static const Color tips = Color(0xFFFFE9EB);
+  static const Color community = Color(0xFFE3EAFD);
 }
