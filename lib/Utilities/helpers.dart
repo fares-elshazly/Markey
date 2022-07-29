5c1389867080138ffd20b1cc4f315d4e01a95cd6
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-07-29 12:14:25 +0200

    Add TimeAgo Helper Method

--- lib/Utilities/helpers.dart
+++ lib/Utilities/helpers.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 abstract class Helpers {
+  static const _placeHolder = '-';
+
   static void setupLogger() {
     Logger.root.level = Level.ALL;
     Logger.root.onRecord.listen((record) {
@@ -14,5 +17,10 @@ abstract class Helpers {
 
   static void dismissKeyboard() {
     FocusManager.instance.primaryFocus?.unfocus();
+  }
+
+  static String timeAgo(DateTime? dateTime) {
+    if (dateTime == null) return _placeHolder;
+    return timeago.format(dateTime);
   }
 }
