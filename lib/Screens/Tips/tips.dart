422664e10cb692cdcc811a0102c9df941500f4e0
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-07-30 06:43:11 +0200

    Add Search Field

--- lib/Screens/Tips/tips.dart
+++ lib/Screens/Tips/tips.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 import '/Widgets/Shared/back_app_bar.dart';
+import '/Widgets/Shared/search_field.dart';
 import '/Widgets/Tips/tip_card.dart';
 import '/Resources/strings.dart';
+import '/Factories/colors_factory.dart';
+import '/Utilities/helpers.dart';
 
 class TipsScreen extends StatelessWidget {
   static const routeName = '/Tips';
@@ -10,13 +13,28 @@ class TipsScreen extends StatelessWidget {
   const TipsScreen({Key? key}) : super(key: key);
 
   final _bodyHorizontalMargin = 15.0;
+  final _contentMargin = 10.0;
   final _internalMargin = 5.0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
-      body: Container(
+      body: _buildBody(context),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return const BackAppBar(
+      title: MRKStrings.tipsTitle,
+      backgroundColor: ColorsFactory.background,
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return GestureDetector(
+      onTap: Helpers.dismissKeyboard,
+      child: Container(
         width: MediaQuery.of(context).size.width,
         padding: EdgeInsets.symmetric(horizontal: _bodyHorizontalMargin),
         child: _buildContent(),
@@ -24,16 +42,28 @@ class TipsScreen extends StatelessWidget {
     );
   }
 
-  PreferredSizeWidget _buildAppBar() {
-    return const BackAppBar(title: MRKStrings.tipsTitle);
+  Widget _buildContent() {
+    return Column(
+      children: [
+        _buildSearch(),
+        SizedBox(height: _contentMargin),
+        _buildTips(),
+      ],
+    );
   }
 
-  Widget _buildContent() {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (BuildContext context, int index) {
-        return _buildTipCard(index);
-      },
+  Widget _buildSearch() {
+    return SearchField();
+  }
+
+  Widget _buildTips() {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          return _buildTipCard(index);
+        },
+      ),
     );
   }
 
