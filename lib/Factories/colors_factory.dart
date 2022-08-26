fb66a3eaf1061b96bb2cb9fe1bf1875aaf167151
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-08-26 12:24:57 +0200

    Add Gradient Colors

--- lib/Factories/colors_factory.dart
+++ lib/Factories/colors_factory.dart
@@ -5,6 +5,8 @@ class ColorsFactory {
   static const Color secondaryBackground = Color(0xFFFFFFFF);
   static const Color primary = Color(0xFF5448B2);
   static const Color secondary = Color(0xFFFFFFFF);
+  static const Color gradientTop = Color(0xFFAC469A);
+  static const Color gradientBottom = Color(0xFF5F3092);
   static const Color text = Color(0xFF000000);
   static const Color secondaryText = Color(0xFF787878);
   static const Color field = Color(0xFFF1F1F1);
@@ -12,7 +14,4 @@ class ColorsFactory {
   static const Color success = Color(0xFF4BB543);
   static const Color danger = Color(0xFFDE0A0A);
   static const Color rate = Color(0xFFFFD700);
-
-  static const Color tips = Color(0xFFFFE9EB);
-  static const Color community = Color(0xFFE3EAFD);
 }
