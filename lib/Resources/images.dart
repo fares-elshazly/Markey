9fb2730cececfd08ead8c7ae4b2195c5f19af50a
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-08-27 13:15:03 +0200

    Remove Unused Images

--- lib/Resources/images.dart
+++ lib/Resources/images.dart
@@ -4,9 +4,6 @@ abstract class MRKImages {
   static const logo = '$_baseUrl/logo.png';
   static const avatar = '$_baseUrl/avatar.png';
 
-  static const tips = '$_baseUrl/tips.png';
-  static const community = '$_baseUrl/community.png';
-
   static const shop1 = '$_baseUrl/shop1.jpg';
   static const shop2 = '$_baseUrl/shop2.jpg';
   static const shop3 = '$_baseUrl/shop3.jpg';
