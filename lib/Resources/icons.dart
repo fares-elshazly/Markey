51d64fd970ec4ae92dbc46e3fabbdd174b442bd6
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-08-27 11:08:11 +0200

    Add Home Bottom Bar Icons Resources

--- lib/Resources/icons.dart
+++ lib/Resources/icons.dart
@@ -1,9 +1,16 @@
 abstract class MRKIcons {
   static const _baseUrl = 'assets/icons';
 
+  static const background = '$_baseUrl/background.svg';
+
   static const google = '$_baseUrl/google.svg';
   static const apple = '$_baseUrl/apple.svg';
   static const facebook = '$_baseUrl/facebook.svg';
   static const instagram = '$_baseUrl/instagram.svg';
   static const twitter = '$_baseUrl/twitter.svg';
+
+  static const bottomShape = '$_baseUrl/bottom-shape.svg';
+  static const tipsters = '$_baseUrl/tipsters.svg';
+  static const mall = '$_baseUrl/mall.svg';
+  static const community = '$_baseUrl/community.svg';
 }
