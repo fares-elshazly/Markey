863a11f7865792462edf82f0441d8131ba84db03
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-10-28 16:43:44 +0200

    Bind Authentication Controller

--- lib/Utilities/controllers_binder.dart
+++ lib/Utilities/controllers_binder.dart
@@ -1,6 +1,10 @@
 import 'package:get/get.dart';
 
+import '/Controllers/authentication_controller.dart';
+
 class ControllersBinder implements Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    Get.put(AuthenticationController());
+  }
 }
