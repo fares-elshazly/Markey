794a14bfdeab8c4ef41b5ea26dbd940dd023f9b7
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-10-28 17:02:24 +0200

    Check If Already logged In

--- lib/Screens/Shared/splash.dart
+++ lib/Screens/Shared/splash.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '/Screens/Shared/home.dart';
 import '/Screens/Authentication/login.dart';
 import '/Resources/images.dart';
 import '/Factories/colors_factory.dart';
+import '/Controllers/authentication_controller.dart';
 
 class SplashScreen extends StatefulWidget {
   static const routeName = '/Splash';
@@ -15,6 +17,8 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  final _authController = Get.find<AuthenticationController>();
+
   final _bodyHorizontalMargin = 15.0;
 
   @override
@@ -45,7 +49,17 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Future<void> _loadData() async {
-    await Future.delayed(const Duration(seconds: 3));
-    Get.toNamed(LoginScreen.routeName);
+    final isLoggedIn = await _loadUser();
+    _navigate(isLoggedIn);
+  }
+
+  Future<bool> _loadUser() async {
+    final isLoggedIn = await _authController.checkLoggedIn();
+    return isLoggedIn;
+  }
+
+  Future<void> _navigate(bool isLoggedIn) async {
+    if (isLoggedIn) return Get.offNamed(HomeScreen.routeName);
+    return Get.offNamed(LoginScreen.routeName);
   }
 }
