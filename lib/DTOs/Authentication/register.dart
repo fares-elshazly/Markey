ba1089040e40f8ab7cdd6974c5cebb0d3da9039b
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-10-28 16:42:00 +0200

    Add Register DTO

---
+++ lib/DTOs/Authentication/register.dart
@@ -0,0 +1,28 @@
+class RegisterDTO {
+  final String username;
+  final String name;
+  final String email;
+  final String password;
+  final String referralCode;
+  final List<int>? tagsIds;
+
+  RegisterDTO({
+    required this.username,
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.referralCode,
+    this.tagsIds,
+  });
+
+  Map<String, dynamic> toJson() => {
+        "username": username,
+        "name": name,
+        "email": email,
+        "password": password,
+        "referral_code": referralCode,
+        "tags": tagsIds,
+        "gender": 1,
+        "status": 1,
+      };
+}
