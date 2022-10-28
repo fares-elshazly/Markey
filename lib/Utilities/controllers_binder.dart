ad79c1a1adcd304f5aef39793ea25ad867730b57
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-10-28 16:44:05 +0200

    Bind Tags Controller

--- lib/Utilities/controllers_binder.dart
+++ lib/Utilities/controllers_binder.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 
 import '/Controllers/authentication_controller.dart';
+import '/Controllers/tags_controller.dart';
 
 class ControllersBinder implements Bindings {
   @override
   void dependencies() {
     Get.put(AuthenticationController());
+    Get.put(TagsController());
   }
 }
