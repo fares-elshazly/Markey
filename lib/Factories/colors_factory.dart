9ad7ed8e08730e4538e434fc713d0e2854222702
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-10-29 07:01:54 +0200

    Add Disabled Color

--- lib/Factories/colors_factory.dart
+++ lib/Factories/colors_factory.dart
@@ -14,5 +14,6 @@ class ColorsFactory {
   static const Color hyperlink = Color(0xFF1777F2);
   static const Color success = Color(0xFF4BB543);
   static const Color danger = Color(0xFFDE0A0A);
+  static const Color disabled = Color(0xFF787878);
   static const Color rate = Color(0xFFFFD700);
 }
