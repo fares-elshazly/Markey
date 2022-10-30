53c2a1a4fca976d81645a2a34174b4a26893cce6
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-10-30 08:05:46 +0200

    Add Community Model

---
+++ lib/Models/Community/community.dart
@@ -0,0 +1,29 @@
+List<Community> communitiesFromJson(List json) =>
+    List<Community>.from(json.map((x) => Community.fromJson(x)));
+
+class Community {
+  final int id;
+  final String title;
+  final String subtitle;
+  final double rate;
+  final String description;
+  final String offer;
+
+  Community({
+    required this.id,
+    required this.title,
+    required this.subtitle,
+    required this.rate,
+    required this.description,
+    required this.offer,
+  });
+
+  factory Community.fromJson(Map<String, dynamic> json) => Community(
+        id: json["id"],
+        title: json["title"],
+        subtitle: json["sub_title"],
+        rate: json["rate"] ?? 0.0,
+        description: json["description"],
+        offer: json["offer"],
+      );
+}
