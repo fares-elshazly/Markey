bc1e0c9840ab7f758aa133c44dd32687f6210346
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-12-16 20:28:02 +0200

    Add Dismissible To Delete Package

--- lib/Widgets/Profile/packages.dart
+++ lib/Widgets/Profile/packages.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '/Factories/text_factory.dart';
 import '/Factories/colors_factory.dart';
+import '/Utilities/progress_indicator.dart';
+import '/Utilities/snackbars.dart';
 import '/Models/Profile/package.dart';
+import '/Models/Shared/message_exception.dart';
+import '/Controllers/authentication_controller.dart';
+import '/Controllers/profile_controller.dart';
 
 class Packages extends StatelessWidget {
   final List<Package> packages;
+  final bool isCurrentUser;
 
-  const Packages({required this.packages, Key? key}) : super(key: key);
+  Packages({
+    required this.packages,
+    this.isCurrentUser = false,
+    Key? key,
+  }) : super(key: key);
+
+  final _authController = Get.find<AuthenticationController>();
+  final _profileController = Get.find<ProfileController>();
 
   final _internalMargin = 5.0;
   final _borderRadius = 8.0;
@@ -19,8 +33,34 @@ class Packages extends StatelessWidget {
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
       itemBuilder: (BuildContext context, int index) {
-        return _buildPackageCard(packages[index]);
+        return _buildContent(packages[index]);
       },
+    );
+  }
+
+  Widget _buildContent(Package package) {
+    if (isCurrentUser) {
+      return _buildDismissiblePackage(package);
+    }
+    return _buildPackageCard(package);
+  }
+
+  Widget _buildDismissiblePackage(Package package) {
+    return Dismissible(
+      key: Key(package.id.toString()),
+      background: _buildDismissibleBackground(),
+      child: _buildPackageCard(package),
+      onDismissed: (_) => _delete(package.id),
+    );
+  }
+
+  Widget _buildDismissibleBackground() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: _internalMargin),
+      decoration: BoxDecoration(
+        color: ColorsFactory.danger,
+        borderRadius: BorderRadius.circular(_borderRadius),
+      ),
     );
   }
 
@@ -82,5 +122,17 @@ class Packages extends StatelessWidget {
 
   Widget _buildPrice(double price) {
     return TextFactory.buildNormalText4('$price £');
+  }
+
+  Future<void> _delete(int id) async {
+    ProgressIndicators.loadingDialog();
+    try {
+      await _profileController.deletePackage(id);
+      await _authController.getProfile();
+      Get.back();
+    } on MessageException catch (error) {
+      Get.back();
+      Snackbars.danger(error.message);
+    }
   }
 }
