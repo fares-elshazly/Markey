db7af24bfad6c655fbf947071b72401c44697400
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-12-16 14:25:07 +0200

    Integrate Profile Services With Controller

---
+++ lib/Controllers/profile_controller.dart
@@ -0,0 +1,68 @@
+import 'package:get/get.dart';
+
+import '/DTOs/Profile/add_previous_work.dart';
+import '/DTOs/Profile/add_certificate.dart';
+import '/DTOs/Profile/add_package.dart';
+import '/Services/Profile/profile_repository.dart';
+
+class ProfileController extends GetxController {
+  late ProfileRepository _repository;
+
+  ProfileController() {
+    _repository = ProfileRepository();
+  }
+
+  Future<bool> addPreviousWork(AddPreviousWorkDTO dto) async {
+    try {
+      final isSuccess = await _repository.addPreviousWork(dto);
+      return isSuccess;
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  Future<bool> deletePreviousWork(int id) async {
+    try {
+      final isSuccess = await _repository.deletePreviousWork(id);
+      return isSuccess;
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  Future<bool> addCertificate(AddCertificateDTO dto) async {
+    try {
+      final isSuccess = await _repository.addCertificate(dto);
+      return isSuccess;
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  Future<bool> deleteCertificate(int id) async {
+    try {
+      final isSuccess = await _repository.deleteCertificate(id);
+      return isSuccess;
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  Future<bool> addPackage(AddPackageDTO dto) async {
+    try {
+      final isSuccess = await _repository.addPackage(dto);
+      return isSuccess;
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  Future<bool> deletePackage(int id) async {
+    try {
+      final isSuccess = await _repository.deletePackage(id);
+      return isSuccess;
+    } catch (error) {
+      rethrow;
+    }
+  }
+}
