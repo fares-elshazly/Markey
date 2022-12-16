f115be69701033f121b4ac52e7fd6c049eb81227
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-12-16 14:24:55 +0200

    Add Profile Services Implementation

---
+++ lib/Services/Profile/profile_repository.dart
@@ -0,0 +1,51 @@
+import 'profile_service.dart';
+
+import '/DTOs/Profile/add_previous_work.dart';
+import '/DTOs/Profile/add_certificate.dart';
+import '/DTOs/Profile/add_package.dart';
+import '/Models/Shared/message_exception.dart';
+
+class ProfileRepository {
+  late ProfileService _profileService;
+
+  ProfileRepository() {
+    _profileService = ProfileService.create();
+  }
+
+  Future<bool> addPreviousWork(AddPreviousWorkDTO dto) async {
+    final response = await _profileService.addPreviousWork(dto.image, dto.userId);
+    if (response.isSuccessful) return true;
+    throw MessageException(response.error.toString());
+  }
+
+  Future<bool> deletePreviousWork(int id) async {
+    final response = await _profileService.deletePreviousWork(id);
+    if (response.isSuccessful) return true;
+    throw MessageException(response.error.toString());
+  }
+
+  Future<bool> addCertificate(AddCertificateDTO dto) async {
+    final response = await _profileService.addCertificate(dto.image, dto.userId);
+    if (response.isSuccessful) return true;
+    throw MessageException(response.error.toString());
+  }
+
+  Future<bool> deleteCertificate(int id) async {
+    final response = await _profileService.deleteCertificate(id);
+    if (response.isSuccessful) return true;
+    throw MessageException(response.error.toString());
+  }
+
+  Future<bool> addPackage(AddPackageDTO dto) async {
+    final body = dto.toJson();
+    final response = await _profileService.addPackage(body);
+    if (response.isSuccessful) return true;
+    throw MessageException(response.error.toString());
+  }
+
+  Future<bool> deletePackage(int id) async {
+    final response = await _profileService.deletePackage(id);
+    if (response.isSuccessful) return true;
+    throw MessageException(response.error.toString());
+  }
+}
