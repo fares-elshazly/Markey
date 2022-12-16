f3df2decefc66c446582a0b72d85b95a77cce3e5
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-12-16 14:24:39 +0200

    Add Profile Services

---
+++ lib/Services/Profile/profile_service.chopper.dart
@@ -0,0 +1,66 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile_service.dart';
+
+// **************************************************************************
+// ChopperGenerator
+// **************************************************************************
+
+// ignore_for_file: always_put_control_body_on_new_line, always_specify_types, prefer_const_declarations, unnecessary_brace_in_string_interps
+class _$ProfileService extends ProfileService {
+  _$ProfileService([ChopperClient? client]) {
+    if (client == null) return;
+    this.client = client;
+  }
+
+  @override
+  final definitionType = ProfileService;
+
+  @override
+  Future<Response<dynamic>> addPreviousWork(String image, int userId) {
+    final $url = 'previous_work';
+    final $parts = <PartValue>[
+      PartValue<int>('user_id', userId),
+      PartValueFile<String>('image', image)
+    ];
+    final $request =
+        Request('POST', $url, client.baseUrl, parts: $parts, multipart: true);
+    return client.send<dynamic, dynamic>($request);
+  }
+
+  @override
+  Future<Response<dynamic>> deletePreviousWork(int id) {
+    final $url = 'previous_work/${id}';
+    final $request = Request('DELETE', $url, client.baseUrl);
+    return client.send<dynamic, dynamic>($request);
+  }
+
+  @override
+  Future<Response<dynamic>> addCertificate(String image, int userId) {
+    final $url = 'certificates';
+    final $request = Request('POST', $url, client.baseUrl);
+    return client.send<dynamic, dynamic>($request);
+  }
+
+  @override
+  Future<Response<dynamic>> deleteCertificate(int id) {
+    final $url = 'certificates/${id}';
+    final $request = Request('DELETE', $url, client.baseUrl);
+    return client.send<dynamic, dynamic>($request);
+  }
+
+  @override
+  Future<Response<dynamic>> addPackage(Map<String, dynamic> body) {
+    final $url = 'packages';
+    final $body = body;
+    final $request = Request('POST', $url, client.baseUrl, body: $body);
+    return client.send<dynamic, dynamic>($request);
+  }
+
+  @override
+  Future<Response<dynamic>> deletePackage(int id) {
+    final $url = 'packages/${id}';
+    final $request = Request('DELETE', $url, client.baseUrl);
+    return client.send<dynamic, dynamic>($request);
+  }
+}
---
+++ lib/Services/Profile/profile_service.dart
@@ -0,0 +1,45 @@
+import 'package:chopper/chopper.dart';
+
+import '/Enums/urls.dart';
+import '/Enums/endpoints.dart';
+import '/Services/Interceptors/authorization_interceptors.dart';
+
+part 'profile_service.chopper.dart';
+
+@ChopperApi()
+abstract class ProfileService extends ChopperService {
+  static ProfileService create() {
+    final client = ChopperClient(
+      baseUrl: Urls.api,
+      services: [
+        _$ProfileService(),
+      ],
+      interceptors: [
+        HttpLoggingInterceptor(),
+        AuthorizationInterceptors.authorizationRequest,
+        AuthorizationInterceptors.authorizationResponse,
+      ],
+      converter: const JsonConverter(),
+    );
+    return _$ProfileService(client);
+  }
+
+  @multipart
+  @Post(path: Endpoints.addPreviousWork)
+  Future<Response> addPreviousWork(@PartFile() String image, @Part('user_id') int userId);
+
+  @Delete(path: Endpoints.deletePreviousWork, optionalBody: true)
+  Future<Response> deletePreviousWork(@Path() int id);
+
+  @Post(path: Endpoints.addCertificate)
+  Future<Response> addCertificate(@PartFile() String image, @Part('user_id') int userId);
+
+  @Delete(path: Endpoints.deleteCertificate, optionalBody: true)
+  Future<Response> deleteCertificate(@Path() int id);
+
+  @Post(path: Endpoints.addPackage)
+  Future<Response> addPackage(@Body() Map<String, dynamic> body);
+
+  @Delete(path: Endpoints.deletePackage, optionalBody: true)
+  Future<Response> deletePackage(@Path() int id);
+}
