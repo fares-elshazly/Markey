9afc81164ae14f350763d0183b4dcc89caf58a71
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-12-17 14:28:15 +0200

    Add Services Services Implementation

---
+++ lib/Services/Services/services_repository.dart
@@ -0,0 +1,38 @@
+import 'services_service.dart';
+
+import '/DTOs/Shared/pagination.dart';
+import '/DTOs/Services/get_experts.dart';
+import '/Models/Services/service.dart';
+import '/Models/Services/experts_list.dart';
+import '/Models/Shared/message_exception.dart';
+
+class ServicesRepository {
+  late ServicesService _servicesService;
+
+  ServicesRepository() {
+    _servicesService = ServicesService.create();
+  }
+
+  Future<List<Service>> services(PaginationDTO dto) async {
+    final pagination = dto.toJson();
+    final response = await _servicesService.services(pagination: pagination);
+    try {
+      final services = servicesFromJson(response.body);
+      return services;
+    } catch (error) {
+      throw MessageException(response.error.toString());
+    }
+  }
+
+  Future<ExpertsList> experts(GetExpertsDTO expertsDto, PaginationDTO paginationDto) async {
+    final servicesIds = expertsDto.servicesIds;
+    final pagination = paginationDto.toJson();
+    final response = await _servicesService.experts(servicesIds, pagination: pagination);
+    try {
+      final experts = ExpertsList.fromJson(response.body);
+      return experts;
+    } catch (error) {
+      throw MessageException(response.error.toString());
+    }
+  }
+}
