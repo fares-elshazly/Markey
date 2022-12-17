8dcb7ecaeb63e1d4ea6e03e9da943db30df8e688
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-12-18 01:37:43 +0200

    Add User Id Property

--- lib/DTOs/Tips/add_tip.dart
+++ lib/DTOs/Tips/add_tip.dart
@@ -2,16 +2,19 @@ class AddTipDTO {
   final String title;
   final String content;
   final List<int> tagsIds;
+  final int userId;
 
   AddTipDTO({
     required this.title,
     required this.content,
     required this.tagsIds,
+    required this.userId,
   });
 
   Map<String, dynamic> toJson() => {
         "title": title,
         "content": content,
         "tags": tagsIds,
+        "user_id": userId,
       };
 }
