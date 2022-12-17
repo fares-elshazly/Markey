708280f81f8145ce587d700c8f13d5a56490e7be
Fares El-Shazly <fares.elshazlyy@gmail.com>
2022-12-17 17:16:19 +0200

    Add Generate Profile Link

--- lib/Enums/urls.dart
+++ lib/Enums/urls.dart
@@ -1,4 +1,8 @@
 abstract class Urls {
   static const baseUrl = 'http://api.markey-eg.com';
   static const api = '$baseUrl/api';
+
+  static const profile = '$baseUrl/view/profile';
+
+  static String generateProfileLink(int userId) => '$profile/$userId';
 }
